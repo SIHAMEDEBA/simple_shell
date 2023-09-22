c8e606b7f5e5146406736069e0d51f012ab08a89
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-09-22 15:03:11 -0700

    3

---
+++ memory.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * bfree - frees a pointer and NULLs address
+ * @ptr: address of pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
